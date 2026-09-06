1d562ad9c09ec5271a685e2cdda79a865e93aedb
Rafael Vieira <vieira.rafa1986@gmail.com>
2026-09-06 14:17:48 -0400

    Esconde temporariamente o Modo Estudo da tela inicial

--- src/App.jsx
+++ src/App.jsx
@@ -150,7 +150,7 @@ function App() {
                     <ul className="space-y-2">
                         <li><a href="#features" className="text-gray-400 hover:text-white transition-colors">Features</a></li>
                         <li><a href="#how-it-works" className="text-gray-400 hover:text-white transition-colors">Como Funciona</a></li>
-                        <li><a href="https://github.com/vieira86" target="_blank" rel="noreferrer" className="text-gray-400 hover:text-white transition-colors">GitHub</a></li>
+                        {/* <li><a href="https://github.com/vieira86" target="_blank" rel="noreferrer" className="text-gray-400 hover:text-white transition-colors">GitHub</a></li> */}
                     </ul>
                 </div>
 
@@ -170,13 +170,13 @@ function App() {
                     </div>
 
                     <div className="flex space-x-4 mt-4">
-                        <a href="https://github.com/vieira86" target="_blank" rel="noreferrer"
+                        {/* <a href="https://github.com/vieira86" target="_blank" rel="noreferrer"
                         className="flex items-center space-x-2 text-gray-400 hover:text-white transition">
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.423 3.297-1.423.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 8.524-6.89 8.524-1.271 0-2.471-.267-3.564-.748l1.416-2.409c1.004.335 2.084.515 3.207.515 5.406 0 9.799-4.393 9.799-9.799 0-5.406-4.393-9.799-9.799-9.799z"/>
                             </svg>
                             <span>GitHub</span>
-                        </a>
+                        </a> */}
                     </div>
                 </div>
             </div>
